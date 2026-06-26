8b4768a169d5d91fbefa0d9bc22b329e13c3e095
anton-borodin-inr <rignar37@gmail.com>
2026-06-26 23:39:07 +0300

    task 4: implement start game flow

---
+++ unity-project/MiniCatcher/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private GameObject mainMenuPanel;
+    [SerializeField] private GameObject gameplayPanel;
+    [SerializeField] private TMP_Text scoreText;
+    [SerializeField] private TMP_Text livesText;
+
+    private int score;
+    private int lives;
+
+    private void Start()
+    {
+        ShowMainMenu();
+    }
+
+    public void StartGame()
+    {
+        score = 0;
+        lives = 3;
+
+        mainMenuPanel.SetActive(false);
+        gameplayPanel.SetActive(true);
+
+        UpdateGameplayUi();
+    }
+
+    private void ShowMainMenu()
+    {
+        score = 0;
+        lives = 3;
+
+        mainMenuPanel.SetActive(true);
+        gameplayPanel.SetActive(false);
+
+        UpdateGameplayUi();
+    }
+
+    private void UpdateGameplayUi()
+    {
+        scoreText.text = $"Score: {score}";
+        livesText.text = $"Lives: {lives}";
+    }
+}
