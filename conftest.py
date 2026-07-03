54ffb98b4fcdd503d675f111a08570e5a8e7706f
anton-borodin-inr <rignar37@gmail.com>
2026-07-03 22:26:12 +0300

    chore: pin pytest 9.0.3, move config to pytest.toml, harden fixture teardown

--- conftest.py
+++ conftest.py
@@ -1,8 +1,12 @@
 import pytest
+
 from helpers.alt_driver import create_driver
+
 
 @pytest.fixture(scope="session")
 def alt_driver():
     driver = create_driver()
-    yield driver
-    driver.stop()
+    try:
+        yield driver
+    finally:
+        driver.stop()
